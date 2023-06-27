cf0743ece73156e5fa6c5116525d977ac6719713
Paul Fritsche <paul.fritsche@lff.bayern.de>
2023-06-27 13:22:05 +0200

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kaycolors/",
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 });
