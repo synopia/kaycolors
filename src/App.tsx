abb17e5d983ab3d350ba6cb6951c8001a8d0fefb
Paul Fritsche <paul.fritsche@lff.bayern.de>
2023-06-27 13:52:03 +0200

    Timer

--- src/App.tsx
+++ src/App.tsx
@@ -52,12 +52,32 @@ function App() {
   const [shown, setShown] = useState<KayColor>("red");
   const [count, setCount] = useState(0);
   const [success, setSuccess] = useState(0);
+  const [time, setTime] = useState(60000.0);
+  const [running, setRunning] = useState(false);
 
   const randomize = useCallback(()=>{
     const idx = Math.floor(Math.random() * 3);
     setShown(KayColors[idx]);
     setColors(_.shuffle(KayColors));
   }, [setShown, setColors]);
+
+  useEffect(()=>{
+    if( running && time<=0 ) {
+      setRunning(false);
+    }
+  }, [time, running]);
+
+  useEffect(()=>{
+    let interval = 0;
+    if( running ) {
+      interval = setInterval(()=>{
+        setTime((time: number)=>time-100);
+      }, 100);
+    } else {
+      clearInterval(interval);
+    }
+    return ()=>clearInterval(interval)
+  }, [running, time]);
 
   useEffect(() => {
     randomize();
@@ -68,13 +88,17 @@ function App() {
       <header className="App-header">
         <div>
           <button onClick={()=>{
-            setCount(0)
-            setSuccess(0)
-            randomize()
+            setCount(0);
+            setSuccess(0);
+            setRunning(false);
+            setTime(60000.0);
+            randomize();
           }}>Reset</button>
         </div>
         <div>
-
+          Time: {time/1000.0}
+        </div>
+        <div>
           Correct: {success} &nbsp;
           Total: {count} &nbsp;
           Rate: {count!==0 ? Math.round(100*success/count): 0}%
@@ -85,11 +109,14 @@ function App() {
         <div>
           {colors.map((color: KayColor) => {
             return <KaySymbol key={color} type={color} size={1} onClick={() => {
+              if( !running ) {
+                setRunning(true);
+              }
               setCount(count + 1);
               if (checkColor(shown, color)) {
                 setSuccess(success + 1);
               }
-              randomize()
+              randomize();
             }} />;
           })}
         </div>
